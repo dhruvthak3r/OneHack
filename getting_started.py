30d785bf20c3ac293ba9e5348a9186422fddaaca
dhruvthak3r <dhruvthaker777@gmail.com>
2025-04-06 22:24:58 +0530

    lets call it a day

--- getting_started.py
+++ getting_started.py
@@ -1,15 +1,23 @@
 import asyncio
-from crawl4ai import AsyncWebCrawler,BrowserConfig
+from crawl4ai import AsyncWebCrawler,BrowserConfig,CrawlerRunConfig,CacheMode
 
 async def crawler():
-    browser_config = BrowserConfig(
-        headless=True,
-        java_script_enabled=True,
+    Browser_config = BrowserConfig(
+        headless=False,
+        browser_type="Edge",
+        cookies=None
     )
-    js_script = window.scrollTo(0, document.body.scrollHeight)
-    browser_config.add_js_script(js_script)
-    async with AsyncWebCrawler(config=browser_config) as crawler: 
-     result = await crawler.arun("https://devfolio.co/hackathons/")
+    crawler_config = CrawlerRunConfig(
+       cache_mode=CacheMode.BYPASS,
+       js_code="window.scrollTo(0, document.body.scrollHeight);",
+       scan_full_page = True,
+       scroll_delay=0.5,
+       adjust_viewport_to_content=True
+    )
+    async with AsyncWebCrawler(config=Browser_config) as crawler:
+     result = await crawler.arun("https://devfolio.co/hackathons/open",config=crawler_config)
+     print(result.status_code)
+     print(result.success)
      print(result.markdown)
      
 
