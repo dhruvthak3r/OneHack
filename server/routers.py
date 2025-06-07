efd2528fc39dee61c28aaebdaeb7d6aeaaf003a4
Dhruv Thaker <dhruvt@senkusha.in>
2025-06-07 17:28:42 +0530

    just checking if git works lmao

--- server/routers.py
+++ server/routers.py
@@ -1,23 +1,19 @@
 from fastapi import FastAPI, Query,Depends,HTTPException
 import uvicorn
 
-
 from typing import Optional,List
-
 
 from sqlalchemy.orm import Session
 
-
 from server.server_utils import get_hackathons_by_platform,lifespan,get_session
-
 
 app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/get-all-hackathons")
 async def get_all_hackathons(session :  Session = Depends(get_session),
-                            platform: Optional[list[str]] = Query(None, alias="p"),
-                            mode: Optional[list[str]] = Query(None)
+                            platform: Optional[List[str]] = Query(None, alias="p"),
+                            mode: Optional[List[str]] = Query(None)
                             ):
     """
     Fetch all hackathons from the database.
@@ -48,7 +44,7 @@ async def get_all_hackathons(session :  Session = Depends(get_session),
 
 @app.get("/get-devfolio")
 async def get_devfolio_hackathons(session: Session = Depends(get_session),
-                                  mode: Optional[list[str]] = Query(None)):
+                                  mode: Optional[List[str]] = Query(None)):
     """
     Fetch Devfolio hackathons from the database.
     """
@@ -63,23 +59,23 @@ async def get_devfolio_hackathons(session: Session = Depends(get_session),
 @app.get("/get-devpost")
 async def get_devpost_hackathons(
     session: Session = Depends(get_session),
-    mode: Optional[list[str]] = Query(None)
+    mode: Optional[List[str]] = Query(None)
 ):
     return await get_hackathons_by_platform(session, "Devpost", mode)
 
 @app.get("/get-unstop")
 async def get_unstop_hackathons(
     session: Session = Depends(get_session),
-    mode: Optional[list[str]] = Query(None)
+    mode: Optional[List[str]] = Query(None)
 ):
     return await get_hackathons_by_platform(session, "Unstop", mode)
 
 @app.get("/get-dorahacks")
 async def get_dorahacks_hackathons(
     session: Session = Depends(get_session),
-    mode: Optional[list[str]] = Query(None)
+    mode: Optional[List[str]] = Query(None)
 ):
-    return await get_hackathons_by_platform(session, "DoraHacks", mode)
+    return await get_hackathons_by_platform(session, "DoraHack", mode)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8080)
