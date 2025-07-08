54623996e83d86ebfae847941b981cf009615e79
Dhruv Thaker <dhruvthaker777@gmail.com>
2025-07-08 18:51:44 +0530

    rate-limiting

--- notifications/receivers/receive_emails.py
+++ notifications/receivers/receive_emails.py
@@ -5,7 +5,8 @@ from notifications.utils import get_connection,get_brevo_headers,get_brevo_paylo
 
 from ratelimit import limits, sleep_and_retry
 
-
+@sleep_and_retry
+@limits(calls=1, period=1) # Limit to 10 calls per minute
 def send_brevo_email(to_email, sender_email, name, email_html_template):
     payload = get_brevo_payload(sender_email, to_email, name, email_html_template)
     headers = get_brevo_headers()
