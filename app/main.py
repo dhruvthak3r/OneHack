f936f1b97d69537f00a541bca0561eb21ae0aeb5
Dhruv Thaker <dhruvthaker777@gmail.com>
2025-07-14 19:46:12 +0530

    cors

--- app/main.py
+++ app/main.py
@@ -5,10 +5,20 @@ import uvicorn
 from app.routers import auth, api
 
 from starlette.middleware.sessions import SessionMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.utils import lifespan
 
 app = FastAPI(lifespan=lifespan)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(api.router, prefix="/api", tags=["api"])
